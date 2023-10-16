f70a30881e29a378028e52871ff54c339cd8a955
matheus-sergipano <146130943+matheus-sergipano@users.noreply.github.com>
2023-10-16 01:52:29 -0300

    Parte de Java Script do jogo da memoria
    Essa foi a base trabalhada https://github.com/YujiYashima/Arthropoda

---
+++ script.js
@@ -0,0 +1,83 @@
+//Definir atalhos
+      const qsa = e => document.querySelectorAll(e);
+
+      //Variaveis globais
+      const cards = qsa('.memory-card');
+      let hasFlippedCard = false;
+      let lockBoard = false;
+      let firstCard, secondCard;
+      let moviments = 0
+
+      //Flip Action
+      function flipCard() {
+
+          if (lockBoard) return;
+          if (this === firstCard) return;
+
+          this.classList.add('flip');
+
+          if (!hasFlippedCard) {
+
+              //first click
+              hasFlippedCard = true;
+              firstCard = this;
+
+              return;
+          }
+
+          //second click
+          secondCard = this;
+
+          checkForMatch();
+
+      }
+
+      const checkForMatch = () => {
+
+          let isMatch = firstCard.dataset.arthropoda == secondCard.dataset.arthropoda;
+
+          isMatch ? disableCards() : unflipCards();
+          isMatch ? document.getElementById("movimentos").innerHTML = moviments: document.getElementById("movimentos").innerHTML = ++moviments
+      }
+
+      const disableCards = () => {
+
+          firstCard.removeEventListener('click', flipCard);
+
+          secondCard.removeEventListener('click', flipCard);
+
+          resetBoard();
+
+      }
+
+      const unflipCards = () => {
+
+          lockBoard = true;
+
+          //not a match
+          setTimeout(() => {
+
+              firstCard.classList.remove("flip");
+              secondCard.classList.remove("flip");
+
+              resetBoard();
+
+          }, 1500); 
+      }
+
+      const resetBoard = () => {
+
+          [hasFlippedCard, lockBoard] = [false, false];
+
+          [firstCard, secondCard] = [null, null];
+      }
+
+      (function shuffle(){
+          cards.forEach(card => {
+              let randomPos = Math.floor(Math.random() * 12);
+
+              card.style.order = randomPos;
+          });
+      })();
+
+      cards.forEach(card => card.addEventListener('click', flipCard));
